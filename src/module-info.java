fdbe035afc8e05d6c55de35a7c3ebc4fecb69436
DilR7 <erictianto12@gmail.com>
2024-12-02 15:37:07 +0700

    Add Database, Model, Controller, View

---
+++ src/module-info.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+/**
+ * @author User
+ *
+ */
+module CaLouselF {
+	requires javafx.graphics;
+	requires javafx.controls;
+	requires java.sql;
+	requires javafx.base;
+	exports model;
+	exports main to javafx.graphics;
+}
